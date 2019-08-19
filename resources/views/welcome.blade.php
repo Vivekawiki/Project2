166508daf7ed0c5b7365c783a7add28f7a30a9e4
Swami Viprananda <viprananda@rkmm.org>
2019-08-19 11:23:56 +0530

    Testing again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Welcome to Vivekawiki.org</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Vivekawiki.org
+                    Welcome to Vivekawiki.org!
                 </div>
 
                 <div class="links">
